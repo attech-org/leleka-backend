72341a577d336d16773286d601f3945b11ae8b75
alexandrtovmach <alexandrtovmach@gmail.com>
2022-08-14 18:50:56 +0200

    docs: Added description for Twitter OAuth

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,6 +3,19 @@ import { register } from "../services/auth.service";
 
 const authRouter = express.Router();
 
+// authRouter.route("/twitter").get(
+  // post request to Twitter API
+  // receive redirectUri in response
+  // redirect your user to received redirectUri res.redirect()
+// );
+
+// authRouter.route("/twitter_callback").get(
+  // in query params: "code"
+  // post reuqest to Twitter API with received "code" and "twitter_client_id", "twitter_client_secret" from .env 
+  // in response you'll have authToken and refreshAuthToken
+  // save them to DB and send it to user
+// );
+
 authRouter.route("/register").post(async (req: Request, res: Response) => {
   try {
     // process input data
