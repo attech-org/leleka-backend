d162b0d68b52e2db0257f036a2102648520098bd
Andrew Kaenko <kaenkoa@icloud.com>
2022-08-17 20:45:49 +0300

    feat: Received access token

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,21 +1,71 @@
 import express, { Request, Response } from "express";
+import superagent from "superagent";
 
 import { register } from "../services/auth.service";
 
 const authRouter = express.Router();
 
-// authRouter.route("/twitter").get(
-// post request to Twitter API
-// receive redirectUri in response
-// redirect your user to received redirectUri res.redirect()
-// );
+const redirectUri = `http://localhost:${process.env.PORT}/api/auth/twitter-callback`;
+const twitterClientId = process.env.TWITTER_CLIENT_ID;
+const twitterClientSecret = process.env.TWITTER_CLIENT_SECRET;
+const state = {};
+const codeChallenge = "sdSD939fFFsfdfdskojilmsionjfje";
 
-// authRouter.route("/twitter_callback").get(
-// in query params: "code"
-// post reuqest to Twitter API with received "code" and "twitter_client_id", "twitter_client_secret" from .env
+// const authOptions = {
+//   method: "POST",
+//   url: "https://api.twitter.com/2/oauth2/token",
+//   headers: {
+//     "Content-Type": "application/x-www-form-urlencoded",
+//   },
+
+//   form: {
+//     grantType: "authorization_code",
+//     clientId: twitterClientId,
+//     clientSecret: twitterClientSecret,
+//     code: code,
+//     redirectUri: redirectUri,
+//   },
+// };
+
+authRouter.route("/twitter").get(async (_req: Request, res: Response) => {
+  res.redirect(
+    `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${twitterClientId}&redirect_uri=${redirectUri}&scope=tweet.read%20users.read%20follows.read%20follows.write&state=${state}&code_challenge=${codeChallenge}&code_challenge_method=plain`
+  );
+});
+
+authRouter
+  .route("/twitter-callback")
+  .get(async (req: Request, res: Response) => {
+    const code = req.query.code;
+    res.sendStatus(200);
+    if (code) {
+      try {
+        const form = {
+          code: code,
+          grant_type: "authorization_code",
+          // client_id: twitterClientId,
+          redirect_uri: redirectUri,
+          code_verifier: codeChallenge,
+        };
+
+        const base64secret = new Buffer(
+          `${twitterClientId}:${twitterClientSecret}`
+        ).toString("base64");
+        const response = await superagent
+          .post("https://api.twitter.com/2/oauth2/token")
+          .set("Content-Type", "application/x-www-form-urlencoded")
+          .set("Authorization", `Basic ${base64secret}`)
+          .send(form);
+        console.info("Request", response.body);
+      } catch (err: unknown) {
+        console.error(err);
+      }
+    }
+  });
+// in query params: "code" +
+// post reuqest to Twitter API with received "code" and "twitter_client_id", "twitter_client_secret" from .env +
 // in response you'll have authToken and refreshAuthToken
 // save them to DB and send it to user
-// );
 
 authRouter.route("/register").post(async (req: Request, res: Response) => {
   try {
