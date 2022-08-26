8a0ffb7c42072857e6cea3714505c8a857bfd342
Vitalii Denysyuk <denvit1@gmail.com>
2022-08-26 16:49:38 +0300

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
 import "express-async-errors";
 
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 app.use(express.json());
 app.use(httpLogger);
-
+app.use(cors());
 app.use("/api", apiRoutes);
 app.use((req: Request, res: Response) => {
   res.status(404);
