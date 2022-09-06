41313ace98b9e7ba6c432bceb58f1f191dc30863
NikPaukov <sirelfarionik@gmail.com>
2022-09-06 21:29:57 +0300

    same changes to likes endpoints

--- src/routes/likes.route.ts
+++ src/routes/likes.route.ts
@@ -1,24 +1,29 @@
 import express, { Request, Response } from "express";
 
+import { isAuthorized } from "../middlewares/isAuthorized.middlewares";
 import { changeLike, getLikeById, updateLike } from "../services/likes.service";
 
 const likesRoutes = express.Router();
 
-likesRoutes.post("/", async (req: Request, res: Response) => {
-  await changeLike(req.body.tweet, req.body.user);
-  res.sendStatus(200);
-});
+likesRoutes
+  .route("/")
+  .post(isAuthorized, async (req: Request, res: Response) => {
+    await changeLike(req.body.tweet, req.body.user._id);
+    res.sendStatus(200);
+  });
 
-likesRoutes.get("/:id", async (req: Request, res: Response) => {
+likesRoutes.route("/:id").get(async (req: Request, res: Response) => {
   const result = await getLikeById(req.params.id);
   res.status(200).send(result);
 });
-likesRoutes.put("/:id", async (req: Request, res: Response) => {
-  const result = await updateLike(req.params.id, {
-    tweet: req.body.tweet,
-    user: req.body.user,
+likesRoutes
+  .route("/:id")
+  .put(isAuthorized, async (req: Request, res: Response) => {
+    const result = await updateLike(req.params.id, {
+      tweet: req.body.tweet,
+      user: req.body.user._id,
+    });
+    res.status(200).send(result);
   });
-  res.status(200).send(result);
-});
 
 export default likesRoutes;
