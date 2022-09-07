f41f92cc71877064337d4422805b732cc2d8a4a4
fominmaksim <fomin.max.13@gmail.com>
2022-09-08 00:48:00 +0200

    feat: implemented requested changes

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,11 +1,8 @@
 import * as yup from "yup";
-import Lazy from "yup/lib/Lazy";
 import { OptionalObjectSchema } from "yup/lib/object";
 import { AnyObject } from "yup/lib/types";
 
-export type AnyYupSchema =
-  | OptionalObjectSchema<AnyObject>
-  | Lazy<OptionalObjectSchema<AnyObject>>;
+export type AnyYupSchema = OptionalObjectSchema<AnyObject>;
 
 export const loginSchema = yup.object({
   username: yup.string().required("Username can't be blank"),
@@ -29,23 +26,7 @@ export const followersSchema = yup.object({
   following: yup.string().required("ownerId can't be blank"),
 });
 
-export const userIdSchema = yup.object({
-  userId: yup.string().required(),
-});
-
-export const tweetIdSchema = yup.object({
-  tweetId: yup.string().required(),
-});
-
-export const likesQuerySchema = yup.lazy((values) => {
-  if (values.userId) {
-    return userIdSchema;
-  } else if (values.tweetId) {
-    return tweetIdSchema;
-  } else {
-    return yup.object({
-      userId: yup.string().required("tweetId or userId are required"),
-      tweetId: yup.string().required("tweetId or userId are required"),
-    });
-  }
+export const likesQuerySchema = yup.object({
+  userId: yup.string(),
+  tweetId: yup.string(),
 });
