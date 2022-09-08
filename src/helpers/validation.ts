d872799d5c12d234eccd4ba0bfe43c9a04ba39ec
alexandrtovmach <alexandrtovmach@gmail.com>
2022-09-09 00:03:14 +0200

    fix; Resolved issue with validation

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -14,14 +14,19 @@ export const loginSchema = yup.object({
   }),
 });
 
-export const registerSchema = loginSchema.concat(
-  yup.object({
+export const registerSchema = yup.object({
+  body: yup.object({
+    username: yup.string().required("Username can't be blank"),
+    password: yup
+      .string()
+      .required("Password can't be blank")
+      .min(8, "Please use minimum of 8 characters for password"),
     email: yup
       .string()
       .required("Email can't be blank")
       .email("Please use a valid email address"),
-  })
-);
+  }),
+});
 
 export const followersSchema = yup.object({
   body: yup.object({
