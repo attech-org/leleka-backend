3e9b90a396ca533645677ba4fd7244767a07d8a2
Vitalii Denysyuk <denvit1@gmail.com>
2022-09-09 11:47:59 +0300

    chore: check url image in link preview

--- src/routes/proxyLinkPreview.route.ts
+++ src/routes/proxyLinkPreview.route.ts
@@ -1,9 +1,27 @@
+import axios from "axios";
 import express from "express";
 
 import validUrl from "../helpers/validUrl";
 import { getMetadata } from "../services/proxyLinkPreview.service";
 
 const proxyLinkPreviewRouter = express.Router();
+
+const checkAccessToImage = async (url: string) => {
+  try {
+    const response = await axios({
+      method: "get",
+      url: url,
+      responseType: "stream",
+    });
+    if ((response.status = 200)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+};
 
 proxyLinkPreviewRouter
   .route("/")
@@ -13,6 +31,9 @@ proxyLinkPreviewRouter
       return res.status(400).json({ error: "Invalid URL" });
     }
     const metadata = await getMetadata(url);
+    if (metadata.image && !(await checkAccessToImage(metadata.image))) {
+      metadata.image = "";
+    }
     res.send({ metadata: metadata });
   });
 
