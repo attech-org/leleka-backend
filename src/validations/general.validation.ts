5f6895368a7951481d3b2f77be00f5d32265e7fc
Alexandr Tovmach <alexandrtovmach@gmail.com>
2022-09-20 01:02:34 +0200

    feat: Added ovveride for simple yup field in query; But it not support original type

--- src/validations/general.validation.ts
+++ src/validations/general.validation.ts
@@ -3,10 +3,22 @@ import { AnyObject, OptionalObjectSchema } from "yup/lib/object";
 
 export type AnyYupSchema = OptionalObjectSchema<AnyObject>;
 
-export const getPaginationSchema = (querySchema: yup.AnyObjectSchema) =>
-  yup.object({
+export const getPaginationSchema = (querySchema: yup.AnyObjectSchema) => {
+  const modifiedQuerySchema: Record<string, yup.AnyObjectSchema> = {};
+  for (const [key, fieldSchema] of Object.entries<yup.AnyObjectSchema>(
+    querySchema.fields
+  )) {
+    if (["string", "number", "date", "boolean"].includes(fieldSchema.type)) {
+      modifiedQuerySchema[key] = yup.object();
+    } else {
+      modifiedQuerySchema[key] = fieldSchema;
+    }
+  }
+
+  return yup.object({
     limit: yup.number(),
     page: yup.number(),
     sort: yup.string(),
-    query: querySchema,
+    query: yup.object(modifiedQuerySchema),
   });
+};
